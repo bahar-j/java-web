30bc78e4ed96c5822ae18a3f966b5fb59272a938
june <joonie811@naver.com>
2021-05-05 00:35:21 +0900

    feat: 주기적으로 서버에 저장된 파일과 데이터베이스 파일 비교해서 삭제

--- board4.0/src/main/java/org/juneco/mapper/BoardMapper.java
+++ board4.0/src/main/java/org/juneco/mapper/BoardMapper.java
@@ -3,6 +3,7 @@ package org.juneco.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.juneco.domain.BoardAttachVO;
 import org.juneco.domain.BoardVO;
 import org.juneco.domain.Criteria;
 
@@ -16,4 +17,5 @@ public interface BoardMapper {
 	public int update(BoardVO board);
 	public int getTotalCount(Criteria cri);
 	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
+	public List<BoardAttachVO> getOldFiles();
 }

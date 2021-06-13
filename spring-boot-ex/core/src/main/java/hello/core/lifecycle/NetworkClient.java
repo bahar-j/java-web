da5fb4335b21ba9781a2b0a930b9e6b57b1a6b64
june <joonie811@naver.com>
2021-06-13 17:55:58 +0900

    빈 생명주기 콜백 예제 추가

---
+++ spring-boot-ex/core/src/main/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,54 @@
+package hello.core.lifecycle;
+
+import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Setter
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+//        connect();
+//        call("초기화 연결 메시지");
+    }
+
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    public void disconnect(){
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        disconnect();
+    }
+
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
+}

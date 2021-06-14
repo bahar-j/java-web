b0a295f824ac465678a56a4463d14c4d7ea3d2f0
june <joonie811@naver.com>
2021-06-14 17:39:16 +0900

    request scope 이용해서 로그 찍기

---
+++ spring-boot-ex/core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,33 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoSerivce;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        // 이 시점에 request bean(MyLogger)이 생성됨. -> uuid, reqeustURL 넣어줌
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        Thread.sleep(1000);
+        logDemoSerivce.logic("testId");
+        return "OK";
+    }
+
+}

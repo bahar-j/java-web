c772909ba8fdf1d89a47b8bf3371c3b80ed59f98
june <joonie811@naver.com>
2021-06-16 16:22:45 +0900

    영속성 컨텍스트 관련 예제 추가

--- jpa-ex/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-ex/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
-
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager entityManager = emf.createEntityManager();
@@ -15,16 +14,37 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            // 비영속
 //            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            // 영속
+//            System.out.println("=== BEFORE ===");
 //            entityManager.persist(member);
-            Member findMember = entityManager.find(Member.class, 1L);
+//            System.out.println("=== AFTER ===");
+//
+//            // 저장할 때 1차 캐시에도 저장하므로 select 쿼리 안날림
+//            Member findMember = entityManager.find(Member.class, 101L);
 //            System.out.println(findMember.getId());
 //            System.out.println(findMember.getName());
-            findMember.setName("HelloJPA");
 
-//            entityManager.remove(findMember);
+            
+//            Member member = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            entityManager.persist(member);
+//            entityManager.persist(member2);
+//            System.out.println("=======================");
+
+
+            Member findMember = entityManager.find(Member.class, 101L);
+            findMember.setName("updatedName");
+
+            Member findAgainMember = entityManager.find(Member.class, 101L);
+            System.out.println(findAgainMember.getName()); // DB 반영 전이지만 1차 캐시에서 가져오니까 변경되어있음!
+
+            //실제로 DB에 쿼리 날리는 시점 - commit 직전
             tx.commit();
         } catch (Exception e){
             tx.rollback();
